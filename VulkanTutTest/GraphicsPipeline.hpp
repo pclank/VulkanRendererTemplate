b4a8c2f57ac6b0116833ebd9ccc6e98e8331ede7
pclank <31415965+pclank@users.noreply.github.com>
2024-11-24 22:12:05 +0100

    refactored all graphics pipeline creation code, and updated wrapper accordingly.

--- VulkanTutTest/GraphicsPipeline.hpp
+++ VulkanTutTest/GraphicsPipeline.hpp
@@ -10,9 +10,58 @@ class GraphicsPipeline {
 public:
 	VkPipeline* pipeline;
 
-	GraphicsPipeline(VkDevice& device, Swapchain& swapChain, const VkSampleCountFlagBits msaaSamples,
+	/// <summary>
+	/// Constructor for vertex, geometry, and fragment shader graphics pipeline
+	/// </summary>
+	/// <param name="device"></param>
+	/// <param name="swapChain"></param>
+	/// <param name="msaaSamples"></param>
+	/// <param name="sampleShading"></param>
+	/// <param name="polygonMode"></param>
+	/// <param name="depthTest"></param>
+	/// <param name="depthWrite"></param>
+	/// <param name="vertexBindingDescriptions"></param>
+	/// <param name="vertexAttributeDescriptions"></param>
+	/// <param name="pipelineLayout"></param>
+	/// <param name="descriptorSetLayout"></param>
+	/// <param name="renderPass"></param>
+	/// <param name="vertShaderFile"></param>
+	/// <param name="fragShaderFile"></param>
+	/// <param name="geomShaderFile"></param>
+	/// <param name="name"></param>
+	/// <param name="graphicsPipeline"></param>
+	GraphicsPipeline(VkDevice& device, Swapchain& swapChain, const VkSampleCountFlagBits msaaSamples, const VkBool32 sampleShading,
+		const VkPolygonMode polygonMode, const VkBool32 const depthTest, VkBool32 depthWrite,
+		const VkVertexInputBindingDescription vertexBindingDescriptions, const std::vector<VkVertexInputAttributeDescription>& vertexAttributeDescriptions,
 		VkPipelineLayout& pipelineLayout, VkDescriptorSetLayout& descriptorSetLayout, VkRenderPass& renderPass,
 		const char* vertShaderFile, const char* fragShaderFile, const char* geomShaderFile, const std::string name,
+		VkPipeline& graphicsPipeline);
+
+	/// <summary>
+	/// Constructor for vertex and fragment shader graphics pipeline
+	/// </summary>
+	/// <param name="device"></param>
+	/// <param name="swapChain"></param>
+	/// <param name="msaaSamples"></param>
+	/// <param name="sampleShading"></param>
+	/// <param name="polygonMode"></param>
+	/// <param name="depthTest"></param>
+	/// <param name="depthWrite"></param>
+	/// <param name="vertexBindingDescriptions"></param>
+	/// <param name="vertexAttributeDescriptions"></param>
+	/// <param name="pipelineLayout"></param>
+	/// <param name="descriptorSetLayout"></param>
+	/// <param name="renderPass"></param>
+	/// <param name="vertShaderFile"></param>
+	/// <param name="fragShaderFile"></param>
+	/// <param name="geomShaderFile"></param>
+	/// <param name="name"></param>
+	/// <param name="graphicsPipeline"></param>
+	GraphicsPipeline(VkDevice& device, Swapchain& swapChain, const VkSampleCountFlagBits msaaSamples, const VkBool32 sampleShading,
+		const VkPolygonMode polygonMode, const VkBool32 depthTest, const VkBool32 depthWrite,
+		const VkVertexInputBindingDescription vertexBindingDescriptions, const std::vector<VkVertexInputAttributeDescription>& vertexAttributeDescriptions,
+		VkPipelineLayout& pipelineLayout, VkDescriptorSetLayout& descriptorSetLayout, VkRenderPass& renderPass,
+		const char* vertShaderFile, const char* fragShaderFile, const std::string name,
 		VkPipeline& graphicsPipeline);
 
 	GraphicsPipeline();
